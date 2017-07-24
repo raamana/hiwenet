1645d46b5c94f771a333755f9ce49e9f511321b6
Pradeep Reddy Raamana <raamana@gmail.com>
2017-07-24 14:17:01 +0100

    Setting up the structure for packaging and distribution

---
+++ hiwenet/__main__.py
@@ -0,0 +1,9 @@
+import hiwenet
+
+def main():
+    "Entry point."
+
+    hiwenet.run()
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='hiwenet',
+      version='0.0.1',
+      description='Histogram-weighted Networks for Feature Extraction and Advance Analysis in Neuroscience',
+      long_description='Histogram-weighted Networks for Feature Extraction and Advance Analysis in Neuroscience',
+      author='Pradeep Reddy Raamana',
+      author_email='raamana@gmail.com',
+      url='https://github.com/raamana/hiwenet',
+      packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]), # ['neuropredict'],
+      install_requires=['numpy', 'pyradigm', 'nibabel'],
+      classifiers=[
+              'Intended Audience :: Science/Research',
+              'Programming Language :: Python',
+              'Topic :: Software Development',
+              'Topic :: Scientific/Engineering',
+              'Operating System :: Microsoft :: Windows',
+              'Operating System :: POSIX',
+              'Operating System :: Unix',
+              'Operating System :: MacOS',
+              'Programming Language :: Python :: 2.7',
+          ],
+      entry_points={
+          "console_scripts": [
+              "hiwenet=hiwenet.__main__:main",
+          ]
+      }
+
+     )
